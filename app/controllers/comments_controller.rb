9f3d99ee2315910e7bc73d7fffda3d8d7c4e188d
Tim Wu <tim7012@msn.com>
2016-06-17 15:10:50 +0800

    add comment CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,61 @@
 class CommentsController < ApplicationController
+
+
+before_action :set_post
+
+
+  def show
+
+  end
+
+
+  def new
+    @comment = @post.comments.build
+
+  end
+
+  def edit
+
+  end
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @comment = @post.comments.find(params[:id])
+    if @comment.udpate(comment_params)
+      redirect_to post_comments_path(@post)
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_comments_path(@post)
+  end
+
+  protected
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
 
+  has_many :comments
   validates_presence_of :title, :content
 end
