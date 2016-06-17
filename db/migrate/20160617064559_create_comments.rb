398bfffa3faa9a308dc9f8067ce4559caf24d6e2
Tim Wu <tim7012@msn.com>
2016-06-17 14:56:03 +0800

    paginate

--- db/migrate/20160617064559_create_comments.rb
+++ db/migrate/20160617064559_create_comments.rb
@@ -1,8 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.text :content
+      t.integer :post_id
 
       t.timestamps null: false
     end
+    add_index :comments, :post_id
   end
 end
